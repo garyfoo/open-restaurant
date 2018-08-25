6063ea86bad50b9b504e3448605eddde66173034
Gary Foo <garyfoo@Garys-WorkBook-Pro.local>
2018-08-25 11:01:00 +0800

    fixed migration protocol error

--- Sources/App/Models/Menu.swift
+++ Sources/App/Models/Menu.swift
@@ -13,10 +13,7 @@ final class Menu: Codable {
     var name: String
     var description: String
     var price: Double
-//    var imageID: ImageItem.ID
-//    var categories: Siblings<MenuItem, Category, MenuItemCategoryPivot> {
-//        return siblings()
-//    }
+    
     init(name: String, description: String, price: Double) {
         self.name = name
         self.description = description
@@ -26,19 +23,5 @@ final class Menu: Codable {
 
 extension Menu: PostgreSQLModel {}
 extension Menu: Content {}
+extension Menu: Migration {}
 extension Menu: Parameter {}
-
-//extension MenuItem {
-//    var user: Parent<MenuItem, ImageItem> {
-//        return parent(\.imageID)
-//    }
-//}
-//
-//extension MenuItem: Migration {
-//    static func prepare(on connection: PostgreSQLConnection) -> Future<Void> {
-//        return Database.create(self, on: connection) { builder in
-//            try addProperties(to: builder)
-//            builder.reference(from: \.imageitemID, to: \ImageItem.id)
-//        }
-//    }
-//}
