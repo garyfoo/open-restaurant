c717cbff58db0c3d7596e2be014f1846af6e140c
Gary Foo <gfmr90@gmail.com>
2018-09-30 22:51:39 +0800

    added psql and update vapor depdencies

--- Sources/App/Controllers/OrderController.swift
+++ Sources/App/Controllers/OrderController.swift
@@ -15,11 +15,43 @@ struct OrderController: RouteCollection {
         orderRoute.post(Order.self, use: orderHandler)
     }
     
-    func orderHandler(_ req: Request, data: Order) throws -> Future<[MenuItem]> {
+    func orderHandler(_ req: Request, data: Order) throws -> Future<PreparationTime> {
         let orderIds = data.menuIds
+//        let waitingTime2 = orderIds.map { orderId in
+//            try MenuItem.query(on: req).filter(\.id == orderId).flatMap { menuItem in
+//                if let category = menuItem?.category {
+//                    self.processOrderTime(category: category)
+//                }
+//            }
+//        }
+        let test = orderIds.map { orderId in
+            return try MenuItem.query(on: req).filter(\.id == orderId).flatMap { menuItem in
+                if let category = menuItem?.category {
+                    self.processOrderTime(category: category)
+                }
+            }
+        }
         return MenuItem.query(on: req).filter(\.id ~~ orderIds).all().map { items in
-            print(items.map { return $0.name })
-            return items
+            let waitingTime = items.map { self.processOrderTime(category: $0.category) }
+            let waitingTimeSum = waitingTime.reduce(0, +)
+            return PreparationTime(prepTime: waitingTimeSum)
+        }
+    }
+    
+    func processOrderTime(category: String) -> Int{
+        switch category {
+        case "appetizers":
+            return 3
+        case "entrees":
+            return 5
+        case "specials":
+            return 15
+        case "drinks":
+            return 5
+        case "desserts":
+            return 5
+        default:
+            return 0
         }
     }
 }
